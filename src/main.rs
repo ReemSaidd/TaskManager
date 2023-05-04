eb4266600c73d22cf4f2ad35ffec0351da33597a
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2023-05-04 14:39:51 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use sysinfo::{ComponentExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt, CpuExt, CpuRefreshKind, RefreshKind};
+// use termion::{color, cursor, style};
 
 fn display_system_information(sys: &System){
 	println!("System name: {}", sys.name().unwrap());
@@ -8,25 +9,58 @@ fn display_system_information(sys: &System){
 	println!("System host name: {}", sys.host_name().unwrap());
 }
 
-fn display_components(sys: &System){
-	for component in sys.components(){
+fn display_components(sys: &mut System) {
+	for component in sys.components() {
 		println!("{:?}", component);
 	}
 }
 
-fn display_hddtemp(sys: &System){
-	for component in sys.components() {
-		if component.label().contains("SSD") || component.label().contains("HDD"){
-			println!("{}: {:?}°C", component.label(), component.temperature());
+fn display_hddtemp(sys: &mut System, arg: &str) {
+	if arg == "" {
+		for component in sys.components_mut() {
+			if component.label().contains("SSD") || component.label().contains("HDD"){
+				println!("{}: {:?}°C", component.label(), component.temperature());
+				component.refresh();
+			}
 		}
-	}	
+
+	}
+	else if arg == "max" {
+		for component in sys.components_mut() {
+			if component.label().contains("SSD") || component.label().contains("HDD"){
+				println!("{}: {:?}°C", component.label(), component.max());
+				component.refresh();
+			}	
+			
+		}
+	}
+	else if arg == "crit"{
+		for component in sys.components_mut() {
+			if component.label().contains("SSD") || component.label().contains("HDD"){
+				println!("{}: {:?}°C", component.label(), component.critical().unwrap());
+				component.refresh();
+			}	
+		}
+	}
+	
 }
 
-fn display_gputemp(sys: &System){
-	for component in sys.components() {
-		if component.label().contains("gpu"){
-			println!("{}: {}°C", component.label(), component.temperature());
-		}
+fn display_gputemp(sys: &mut System, arg: &str) {
+	if arg == "" {
+		for component in sys.components_mut() {
+			if component.label().contains("gpu") {
+				println!("{}: {}°C", component.label(), component.temperature());
+				component.refresh();
+			}
+		}		
+	}
+	else if arg == "max" {
+		for component in sys.components_mut() {
+			if component.label().contains("gpu"){
+				println!("{}: {}°C", component.label(), component.max());
+				component.refresh();
+			}
+		}		
 	}
 }
 
@@ -35,16 +69,34 @@ fn main() {
 	//let networks = sys.networks();
 	loop{
 		let mut input = String::new();
+		let mut arg: &str = "";
 		io::stdin().read_line(&mut input).expect("Failed to get input.");
-		match input.as_str().trim(){
+
+		match input.as_str().trim() {
 			"sysname" => {println!("{}", sys.name().unwrap())},
 			"uname" => {println!("{}", sys.kernel_version().unwrap())},
 			"release" => {println!("{}", sys.os_version().unwrap())},
 			"hostname" => {println!("{}", sys.host_name().unwrap())},
 			"sysinfo" => {display_system_information(&sys)},
-			"hddtemp" => {display_hddtemp(&sys)},
-			"gputemp" => {display_gputemp(&sys)},
-			"sensors" => {display_components(&sys)},
+			"hddtemp" => {
+				display_hddtemp(&mut sys, arg);
+			},
+			"hddtemp -max" => {
+				arg = "max";
+				display_hddtemp(&mut sys, arg);
+			},
+			"hddtemp -crit" => {
+				arg = "crit";
+				display_hddtemp(&mut sys, arg);
+			},
+			"gputemp" => {
+				display_gputemp(&mut sys, arg)
+			},
+			"gputemp -max" => {
+				arg = "max";
+				display_gputemp(&mut sys, arg);
+			},
+			"sensors" => {display_components(&mut sys)},
 			"quit" => {
 				println!("Exiting program");
 				break;
