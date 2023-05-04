bf5651ae9a36538a233136a9b31821b728665f30
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2023-05-04 20:32:05 +0300

    third commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use std::thread;
+use std::time::Duration;
 use sysinfo::{ComponentExt, NetworkExt, NetworksExt, ProcessExt, System, SystemExt, CpuExt, CpuRefreshKind, RefreshKind};
-// use termion::{color, cursor, style};
+use termion::{color, cursor, style};
+use std::io::Write;
 
-fn display_system_information(sys: &System){
+fn display_system_information(sys: &System) {
 	println!("System name: {}", sys.name().unwrap());
 	println!("System kernel version: {}", sys.kernel_version().unwrap());
 	println!("System OS version: {}", sys.os_version().unwrap());
@@ -15,29 +18,66 @@ fn display_components(sys: &mut System) {
 	}
 }
 
+// fn monitor_components(sys: &mut System) {
+// 	loop {
+// 		for component in sys.components() {
+// 			println!("{:?}", component);
+// 		}
+// 	}
+// }
+
+fn monitor_hddtemp(sys: &mut System) {
+	let mut stdout = io::stdout();
+	loop {
+		write!(stdout, "{}", cursor::Left(100)).unwrap();
+		for component in sys.components_mut() {
+			if component.label().contains("SSD") || component.label().contains("HDD"){
+				write!(stdout, "{}{}{}: {:?}°C{}", termion::clear::CurrentLine, color::Fg(color::Red), component.label(), component.temperature(), color::Fg(color::Reset),).unwrap();
+				component.refresh();
+				stdout.flush().unwrap();
+				thread::sleep(Duration::from_millis(200));
+			}
+		}
+
+	}
+}
+
 fn display_hddtemp(sys: &mut System, arg: &str) {
 	if arg == "" {
 		for component in sys.components_mut() {
 			if component.label().contains("SSD") || component.label().contains("HDD"){
-				println!("{}: {:?}°C", component.label(), component.temperature());
+				if component.temperature() < component.critical().unwrap() {
+					//println!("{}{}This is a {}colored{} line", color::Fg(color::Red), style::Bold, color::Fg(color::Yellow), style::Reset);
+					println!("{}{}: {:?}°C{}", color::Fg(color::Green), component.label(), component.temperature(), style::Reset);
+				}
+				else {
+					println!("{}{}: {:?}°C{}", color::Fg(color::Red), component.label(), component.temperature(), style::Reset);
+				}
 				component.refresh();
+				
 			}
 		}
 
 	}
 	else if arg == "max" {
 		for component in sys.components_mut() {
-			if component.label().contains("SSD") || component.label().contains("HDD"){
-				println!("{}: {:?}°C", component.label(), component.max());
-				component.refresh();
-			}	
+			if component.max() < component.critical().unwrap() {
+				if component.label().contains("SSD") || component.label().contains("HDD"){
+					println!("{}{}: {:?}°C{}", color::Fg(color::Green), component.label(), component.max(), style::Reset);
+				}
+				else {
+					println!("{}{}: {:?}°C{}", color::Fg(color::Red), component.label(), component.max(), style::Reset);
+				}
+				component.refresh();	
+			}
+			
 			
 		}
 	}
 	else if arg == "crit"{
 		for component in sys.components_mut() {
 			if component.label().contains("SSD") || component.label().contains("HDD"){
-				println!("{}: {:?}°C", component.label(), component.critical().unwrap());
+				println!("{}{}: {:?}°C{}", color::Fg(color::Red), component.label(), component.temperature(), style::Reset);
 				component.refresh();
 			}	
 		}
@@ -61,6 +101,21 @@ fn display_gputemp(sys: &mut System, arg: &str) {
 				component.refresh();
 			}
 		}		
+	}
+}
+
+fn monitor_gputemp(sys: &mut System) {
+	let mut stdout = io::stdout();
+	loop {
+		write!(stdout, "{}", cursor::Left(100)).unwrap();
+		for component in sys.components_mut() {
+			if component.label().contains("gpu") {
+				write!(stdout, "{}: {:?}°C", component.label(), component.temperature()).unwrap();
+				component.refresh();
+				stdout.flush().unwrap();
+				thread::sleep(Duration::from_millis(200));
+			}
+		}	
 	}
 }
 
@@ -89,8 +144,14 @@ fn main() {
 				arg = "crit";
 				display_hddtemp(&mut sys, arg);
 			},
+			"hddtemp -mon" => {
+				monitor_hddtemp(&mut sys);
+			},
 			"gputemp" => {
 				display_gputemp(&mut sys, arg)
+			},
+			"gputemp -mon" => {
+				monitor_gputemp(&mut sys);
 			},
 			"gputemp -max" => {
 				arg = "max";
@@ -107,4 +168,3 @@ fn main() {
 		}
 	}
 }
-
